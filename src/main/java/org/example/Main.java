942c69a661c1b5c93c048aa292b05a2994c7c145
Ilya Dubenok <dubenokilya@gmail.com>
2023-05-29 21:01:26 +0300

    added db connector

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import org.example.dao.db.ds.DataSourceConnector;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
 public class Main {
+
+    public static void main(String[] args) {
+        DataSource ds = DataSourceConnector.getInstance().getDataSource();
+
+
+    }
 }
