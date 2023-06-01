debe3121f8912b95272369a6ab943d805b15e8ed
Aleksandr Andreev <alexalexov61@gmail.com>
2023-06-01 23:24:29 +0300

    Добавлена логика в AddRatesForRangeService

--- src/main/java/org/example/service/AddRatesForRangeService.java
+++ src/main/java/org/example/service/AddRatesForRangeService.java
@@ -7,6 +7,7 @@ import org.example.service.api.IValidationService;
 import org.example.service.factory.NBRBServiceFactory;
 import org.example.service.factory.ValidationCurrencyServiceFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -16,25 +17,46 @@ public class AddRatesForRangeService implements IAddRatesForRangeService {
 
     @Override
     public List<CurrencyDTO> save(RateRangeDTO rateRange, Boolean showOnlyNew) {
+
+        List<CurrencyDTO> currencies;
+
+        if (hasRatesForPeriod(rateRange)) {
+            if (showOnlyNew){
+                currencies = new ArrayList<>();
+            } else {
+                //TODO Возвращать данные из БД
+                currencies = NBRBServiceFactory.getInstance().getCurrency(rateRange);
+            }
+        } else {
+            if (showOnlyNew){
+                //TODO Сохранение в базу, в currencies список из базы
+                currencies = NBRBServiceFactory.getInstance().getCurrency(rateRange);
+            } else {
+                //TODO Сохранение в базу, в currencies список всех из NBRBService
+                currencies = NBRBServiceFactory.getInstance().getCurrency(rateRange);
+            }
+        }
+
+        return currencies;
+    }
+
+    @Override
+    public List<CurrencyDTO> save(RateRangeDTO rateRange) {
+        return save(rateRange, false);
+    }
+
+    private void validate(RateRangeDTO rateRange){
         validationService.validateTypeCurrency(rateRange.getCurrencyName());
 
         //TODO изменить после правки validateDate()
         validationService.validateDate(rateRange.getBeginDate().toString());
         validationService.validateDate(rateRange.getEndDate().toString());
         //TODO
-
-        if (validationService.hasRatesForPeriod(rateRange.getCurrencyName(),
-                rateRange.getBeginDate(),
-                rateRange.getEndDate()))
-        {
-            //TODO Возвращать данные из БД
-        }
-
-        return NBRBServiceFactory.getInstance().getCurrency(rateRange);
     }
 
-    @Override
-    public List<CurrencyDTO> save(RateRangeDTO rateRange) {
-        return save(rateRange, false);
+    private boolean hasRatesForPeriod(RateRangeDTO rateRange){
+        return validationService.hasRatesForPeriod(rateRange.getCurrencyName(),
+                rateRange.getBeginDate(),
+                rateRange.getEndDate());
     }
 }
