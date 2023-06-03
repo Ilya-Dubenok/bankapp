5ce78ae729010aca00452a8e0165e03571235bc5
Aleksandr Andreev <alexalexov61@gmail.com>
2023-06-03 17:37:25 +0300

    Перенес валидацию в AddRatesForRangeService

--- src/main/java/org/example/entrypoint/web/AddRatesForRangeServlet.java
+++ src/main/java/org/example/entrypoint/web/AddRatesForRangeServlet.java
@@ -1,45 +1,52 @@
 package org.example.entrypoint.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.example.core.dto.RateRangeCreateDTO;
 import org.example.core.dto.CurrencyDTO;
+import org.example.core.dto.RateRangeDTO;
 import org.example.service.api.IAddRatesForRangeService;
 import org.example.service.factory.AddRatesForRangeServiceFactory;
+import org.example.service.factory.DateTimeFormatterFactory;
 import org.example.service.factory.ObjectMapperFactory;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @WebServlet("/add_rates")
 public class AddRatesForRangeServlet extends HttpServlet {
     private final IAddRatesForRangeService service = AddRatesForRangeServiceFactory.getInstance();
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         ObjectMapper mapper = ObjectMapperFactory.getInstance();
-        String jsonResponse;
+        DateTimeFormatter formatter = DateTimeFormatterFactory.getInstance();
 
         String currencyName = req.getParameter("name");
-        String beginDate = req.getParameter("beginDate");
-        String endDate = req.getParameter("endDate");
+        LocalDate beginDate, endDate;
+
+        try{
+            beginDate = LocalDate.parse(req.getParameter("beginDate"), formatter);
+            endDate = LocalDate.parse(req.getParameter("endDate"), formatter);
+        } catch (DateTimeParseException e){
+            resp.getWriter().write(mapper.writeValueAsString(e));
+            return;
+        }
+
         boolean showOnlyNew = false;
         if (req.getParameterMap().containsKey("showOnlyNew")){
             showOnlyNew = Boolean.parseBoolean(req.getParameter("showOnlyNew"));
         }
 
         try {
-            List<CurrencyDTO> rates = service.save(new RateRangeCreateDTO(currencyName, beginDate, endDate), showOnlyNew);
-            jsonResponse = mapper.writeValueAsString(rates);
+            List<CurrencyDTO> rates = service.save(new RateRangeDTO(currencyName, beginDate, endDate), showOnlyNew);
+            resp.getWriter().write(mapper.writeValueAsString(rates));
         } catch (IllegalArgumentException e){
-            jsonResponse = mapper.writeValueAsString(e);
+            resp.getWriter().write(mapper.writeValueAsString(e));
         }
-
-        resp.getWriter().write(jsonResponse);
     }
-
-
 }
